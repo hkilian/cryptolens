8878ab93a1e23060249be3a5f1312861c605aa60
Harry Kilian <harrykilian@gmail.com>
2017-10-02 23:51:20 +0100

    Started using an MVC patturn for interface

--- __main__.py
+++ __main__.py
@@ -11,7 +11,7 @@ if os.path.exists('test.db'):
 	True
 
 from display.displaycommon import *
-from display.displayhome import DisplayHome
+from display.displayhome import *
 from display.displaymarket import DisplayMarket
 from initdb import *
 from exchanges.bitfinex import Bitfinex
@@ -30,6 +30,7 @@ palette = [
 	('change ', 'dark green', ''),
 	('change negative', 'dark red', '')]
 
+# Main loop
 loop = asyncio.get_event_loop()
 
 display = DisplayHome()
@@ -40,8 +41,11 @@ display.PrepareBody()
 header = Header()
 footer = Footer()
 
+# MVC test
+controller = HomeController()
+
 # Create the view
-view = urwid.Frame(header=header, body=display.body, footer=footer)
+view = urwid.Frame(header=header, body=controller.view, footer=footer)
 view = urwid.Padding(view, left=2, right=2)
 
 # Run initdb if missing db
@@ -51,19 +55,23 @@ if not Asset.table_exists():
 else:
 	display.Update("Database already updated")
 
+@asyncio.coroutine
+def slow_operation():
+	while True:
+		bitfinex = Bitfinex()
+		data = bitfinex.PullData()
+
+		view.original_widget.body = urwid.Filler(urwid.Text(str(data['price'])))
+
+		yield from asyncio.sleep(1)
+
+#asyncio.Task(slow_operation())
+
 def rPressed():
 	display = DisplayMarket()
 	display.ShowOrderBook()
 	display.PrepareBody()
 	view.original_widget.body = display.body
-
-def constantUpdate():
-
-	bitfinex = Bitfinex()
-	data = bitfinex.PullData()
-
-	display.Update("Bitcoin price is " + str(data['price']))
-	loop.call_later(1, constantUpdate)
 	
 def input_filter(keys, raw):
 	if 'q' in keys or 'Q' in keys:
@@ -72,10 +80,12 @@ def input_filter(keys, raw):
 	if 'r' in keys or 'R' in keys:
 		rPressed()
 
-
 # Main urwid loop
 urwid_loop = urwid.MainLoop(view, palette,
  						input_filter=input_filter,
  						event_loop=urwid.AsyncioEventLoop(loop=loop))
+
+controller.set_loop(urwid_loop)
+
 urwid_loop.run()
 
