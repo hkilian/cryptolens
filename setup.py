88382cc9a93b4ff712c61ecfc9a1353af657c69b
Harry Kilian <harrykilian@gmail.com>
2017-10-11 00:48:54 +0100

    Modified setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,49 @@
-from distutils.core import setup
+from setuptools import setup, find_packages  # Always prefer setuptools over distutils
+from codecs import open  # To use a consistent encoding
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the relevant file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
-  name = 'cryptolens',
-  packages = ['cryptolens'], 
-  version = '0.1',
-  description = 'Command line tool to monitor the prices of cryptocurrencies',
-  author = 'Harry Kilian',
-  author_email = 'harrykilian@gmail.com',
-  url = 'https://github.com/hkilian/cryptolens',
-  download_url = 'https://github.com/hkilian/cryptolens/archive/0.1.tar.gz',
-  keywords = ['cli', 'cryptocurrency', 'bitcoin'], 
-  classifiers = [],
+
+    name='cryptolens',
+    version='0.0',
+    description = 'Command line tool to monitor the prices of cryptocurrencies',
+    url='https://github.com/hkilian/cryptolens',
+    author = 'Harry Kilian',
+    author_email = 'harrykilian@gmail.com',
+    license='MIT',
+
+    install_requires=[
+          'urwid',
+          'sortedcontainers',
+          'fuzzywuzzy',
+          'websockets',
+      ],
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Utilities',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+
+    # What does your project relate to?
+    keywords='sample setuptools development',
+
+    entry_points={
+       'console_scripts': [
+           'cryptolens = cryptolens.__main__:main',
+           'crypto = cryptolens.__main__:main',
+       ],
+    },
+
+    packages=["cryptolens"],
+
 )
-
-
-
-
-
-
 
