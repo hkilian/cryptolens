eaf45d2dc70ff5c823fbbacb01f2c51b62331f5f
hkilian <harrykilian@gmail.com>
2017-10-11 17:54:24 +0100

    Made changes to setup.py, now installs required packages

--- setup.py
+++ setup.py
@@ -2,27 +2,27 @@ from setuptools import setup, find_packages  # Always prefer setuptools over dis
 from codecs import open  # To use a consistent encoding
 from os import path
 
-here = path.abspath(path.dirname(__file__))
-
-# Get the long description from the relevant file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
-
 setup(
 
     name='cryptolens',
-    version='0.0',
+    version='0.1.11',
     description = 'Command line tool to monitor the prices of cryptocurrencies',
     url='https://github.com/hkilian/cryptolens',
     author = 'Harry Kilian',
     author_email = 'harrykilian@gmail.com',
     license='MIT',
 
+    python_requires='>=3',
+
+    include_package_data=True,
+
     install_requires=[
           'urwid',
           'sortedcontainers',
           'fuzzywuzzy',
           'websockets',
+          'requests',
+          'peewee',
       ],
 
     classifiers=[
@@ -31,6 +31,11 @@ setup(
         'Topic :: Utilities',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
     ],
 
     # What does your project relate to?
@@ -43,7 +48,7 @@ setup(
        ],
     },
 
-    packages=["cryptolens"],
+    packages=find_packages(),
 
 )
 
